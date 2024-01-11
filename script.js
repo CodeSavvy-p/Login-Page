4547c611781db0497363981b6cd4ab93e332b37f
CodeSavvy-p <155844428+CodeSavvy-p@users.noreply.github.com>
2024-01-11 12:04:16 +0530

    files added

---
+++ script.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var rememberMeContainer = document.querySelector('.remember-me');
+    var checkbox = document.querySelector('input[type="checkbox"]');
+    var checkboxCustom = document.querySelector('.checkbox-custom');
+
+    rememberMeContainer.addEventListener('click', function(event) {
+        if(event.target != checkbox) {
+            checkbox.checked = !checkbox.checked;
+            var changeEvent = new Event('change', {
+                'bubbles': true,
+                'cancelable': true
+            });
+            checkbox.dispatchEvent(changeEvent);
+        }
+    });
+    checkbox.addEventListener('change', function(){
+
+    });
+    var form = document.querySelector('.login-form');
+    form.addEventListener('submit', function(event){
+        event.preventDefault;
+    });
+});
